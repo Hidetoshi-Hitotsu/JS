4518ecf2396d3949126480a58f5b8bf8b3597abb
EC2 Default User <ec2-user@ip-172-31-45-148.ap-southeast-2.compute.internal>
2023-04-20 12:47:38 +0000

    JS課題

---
+++ calculator.js
@@ -0,0 +1,48 @@
+
+
+function clickbutton(btn){
+  let result = document.getElementById("result");
+  let target_value = btn.innerHTML;
+  
+  if(target_value == "AC"){
+    result.innerHTML = 0;
+  }else if( target_value == "="){
+    result.innerHTML = eval(result.innerHTML);
+  }else if( target_value == 00){
+    if(result.innerHTML == 0){
+      result.innerHTML = 0;
+    }else{
+      result.innerHTML += target_value;
+    }
+  }else if( target_value == "+" || target_value == "-" || target_value == "*" || target_value == "/" || target_value == "."){
+    var check = String(result.innerHTML).slice(-1);
+      if( check == "+" || check == "-" || check == "*" || check == "/" || check == "."){ 
+        var resultcheck = String(result.innerHTML).slice(0,-1);
+        result.innerHTML = resultcheck + target_value;
+      }else{
+        result.innerHTML += target_value;
+      }
+  }else{
+    if( result.innerHTML == 0){
+      result.innerHTML = target_value;
+    }else{
+      result.innerHTML += target_value;
+    }
+  }
+}
+
+
+$(document).ready(function(){
+  
+  $("#dotbutton").click(function(){
+    $("#dotbutton").prop('disabled',true);
+  });
+  
+  $("#acbutton").click(function(){
+    $("#dotbutton").prop('disabled',false);
+  });
+  
+  $("#equalbutton").click(function(){
+    $("#dotbutton").prop('disabled',false);
+  });
+});
